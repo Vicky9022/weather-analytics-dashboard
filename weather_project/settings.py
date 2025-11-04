0f4c931a236df10b1e2c544785630726b6d1a2c7
Vicky9022 <vikasrathod90222@gmail.com>
2025-11-04 22:10:12 +0530

    Fix CORS_ALLOWED_ORIGINS format

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -124,9 +124,10 @@ REST_FRAMEWORK = {
 # CORS
 
 CORS_ALLOWED_ORIGINS = [
-"http://localhost:3000",
-"[http://127.0.0.1:3000](http://127.0.0.1:3000)",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
+
 
 # Security for production
 
